3d85f053e09426e3311f107f4dcc76a1bef79aae
Shubham Phadte <shubhamphadte37@gmail.com>
2024-06-12 23:42:11 +0530

    Show/ hide title using useState hook in App.js initially, will be structured properly in future

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [showTitle, setShowTitle] = useState(true);
+
+  const toggleTitle = () => {
+    setShowTitle(!showTitle);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={toggleTitle}>Show/ Hide Me</button>
+      {showTitle && <h1>React Challenges</h1>}
     </div>
   );
 }
