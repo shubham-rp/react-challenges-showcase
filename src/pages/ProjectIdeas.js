4da94c14da222e91a85ac9a13ba551bb885d0850
Shubham Phadte <shubhamphadte37@gmail.com>
2024-06-19 23:42:41 +0530

    file changes

--- src/pages/ProjectIdeas.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ProjectIdeas = () => {
-  return <div>ProjectIdeas</div>;
-};
-
-export default ProjectIdeas;
---
+++ src/pages/ProjectIdeasAndResources.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProjectIdeasAndResources = () => {
+  return <div>ProjectIdeas Here we will list project Ideas and resources</div>;
+};
+
+export default ProjectIdeasAndResources;
